3ff94d76701fa940bef870b683e79f8a71869fd1
adedotun <adedotunolawale@gmail.com>
2019-05-12 21:06:03 +0100

    ..'

--- components/Layout/Admin/SideBar.vue
+++ components/Layout/Admin/SideBar.vue
@@ -73,7 +73,7 @@
             </a>
             </li> -->
             <li class="nav-item">
-            <a class="nav-link " @click="logoutUser">
+            <a class="nav-link">
                 <i class="material-icons">logout</i>
                 <span><strong>Logout</strong></span>
             </a>
