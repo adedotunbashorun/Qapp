bee93f3b031fa5d0508c8fcc38ed2818a6c6db6b
user.name : adedotunbashorun <user.email : adedotunolawale@gmail.com>
2019-07-18 14:19:24 +0100

    ..

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
-    // If the user is authenticated redirect to home page
-    if (store.state.auth.token != '') {
-        return redirect('/admin/dashboard')
-    }
+  // If the user is authenticated redirect to home page
+  if (store.state.auth.token && store.state.auth.user.user_type == 'admin') {
+      return redirect('/admin/dashboard')
+  }
 }
